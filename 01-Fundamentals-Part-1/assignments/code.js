ee1404377d2609dc5ab6c6cea703bb98aa1d7b98
Chris <chris.kraus.dev@gmail.com>
2020-11-24 22:09:43 +0100

    second commit

---
+++ 01-Fundamentals-Part-1/assignments/code.js
@@ -0,0 +1,22 @@
+// Values and Variables
+
+var country = 'Poland'
+var continent = 'Europe'
+var population = 38.1
+
+console.log('Values and Variables:', country, continent, population);
+
+// Data types
+
+var isPoland = true
+var language = 
+
+console.log(typeof isPoland)
+console.log(typeof population)
+console.log(typeof country)
+console.log(typeof language)
+
+// let, const and var
+
+var language = 'polish'
+// const continent = 'Europe'
---
+++ 01-Fundamentals-Part-1/starter/challenge.js
@@ -0,0 +1,18 @@
+// const MarkWeight = 78;
+// const JohnWeight = 92;
+// const MarkHight = 1.69;
+// const JohnHight =1.95;
+
+const MarkWeight = 95;
+const JohnWeight = 85;
+const MarkHight = 1.88;
+const JohnHight =1.76;
+
+const MarkBMI = MarkWeight / MarkHight * MarkHight;
+const JohnBMI = JohnWeight / JohnHight * JohnHight;
+
+
+console.log(MarkBMI);
+console.log(JohnBMI);
+const MarkHigherBMI = MarkBMI > JohnBMI;
+console.log(MarkHigherBMI);
