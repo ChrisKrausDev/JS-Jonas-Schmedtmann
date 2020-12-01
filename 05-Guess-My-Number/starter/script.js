7968b10ff6a7add4bb250a099d1b6712cde74199
Chris <chris.kraus.dev@gmail.com>
2020-12-01 21:14:01 +0100

    section_7 video_82

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,38 +1,58 @@
 'use strict';
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
-document.querySelector('.number').textContent = secretNumber;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highscore = 0;
+
+const displayMessage = function(message) {
+    document.querySelector('.message').textContent = message;
+}
 
 document.querySelector('.check').addEventListener('click', function() {
     const guess = Number(document.querySelector('.guess').value);
     console.log(guess, typeof guess);
 
+// Again btn event!  
+document.querySelector('.again').addEventListener('click', function() {
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    score = 20;
+    document.querySelector('.score').textContent = score;
+    displayMessage('Start guessing...');
+    document.querySelector('.number').textContent = "?";
+    document.querySelector('.number').style.width = "15rem";
+    document.querySelector('body').style.backgroundColor = "#222";
+    document.querySelector('.guess').value = "";
+})
 
+// When there is no input
     if (!guess) {
-        document.querySelector('.message').textContent = 'No number';
+        displayMessage('No number!');
+
+// Whe player wins
     } else if (guess === secretNumber) {
-        document.querySelector('.message').textContent = 'Correct Number!'
-    } else if (guess > secretNumber) {
-        if (score > 1) {
-            document.querySelector('.message').textContent = 'Too high!';
-            score--;
-            document.querySelector('.score').textContent = score;
-        } else {
-            document.querySelector('.message').textContent = 'You lost the game!';
-            document.querySelector('.score').textContent = 0;
+        displayMessage('Correct Number!');
+        document.querySelector('.number').textContent = secretNumber;
+
+        document.querySelector('body').style.backgroundColor = '#60b347';
+
+        document.querySelector('.number').style.width = "30rem";
+
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
         }
-    } else if (guess < secretNumber) {
+// When guess is wrong
+    } else if (guess !== secretNumber) {
         if (score > 1) {
-            document.querySelector('.message').textContent = 'Too low!';
+            displayMessage(guess > secretNumber ? 'Too high!' : 'To low!');
             score--;
             document.querySelector('.score').textContent = score;
         } else {
-            document.querySelector('.message').textContent = 'You lost the game!';
+            displayMessage('You loose the game!');
             document.querySelector('.score').textContent = 0;
         }
     }
-});
+}); 
 
 
 // document.querySelector('.message').textContent = 'Correct Number!';
--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,41 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+    console.log('Button clicked');
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) 
+    btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+    // odwołanie do funkcji bez (), z nawiasami wywoła się od razu, bez nawiasów dopiero po wystąpieniu clicka
+
+document.addEventListener('keydown', function(e) {
+    console.log(e);
+    console.log(e.key);
+
+    if (e.key === 'Escape') {
+        if(!modal.classList.contains('hidden')) {
+            closeModal();
+        }
+        // jeżeli modal nie ma klasy hidden (bo zaprzeczenie - !), to zamknij modal (tutaj funkcja koniecznie z () ) 
+
+        // zagnieżdżenie instrukcji warunkowych, lepiej napisać to w ten sposób: 
+     if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
+        }
+    }
+});
